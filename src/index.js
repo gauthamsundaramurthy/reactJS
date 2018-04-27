143549ab33b2ebff887bf4eede98910f0dedcf56
Gautham.S <gautham.s@tringapps.com>
2018-04-27 11:24:31 +0530

    State

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './componentLifeCycle/ComponentLifeCycle';
+import State from './State/State';
+
+ReactDOM.render(<State />, document.getElementById('root'));
