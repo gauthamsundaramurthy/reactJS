9b0bb9c808d606496ba1556d5715672bfb3e01e7
Gautham.S <gautham.s@tringapps.com>
2018-04-27 12:09:18 +0530

    Sending data from child to parent

--- src/index.js
+++ src/index.js
@@ -7,5 +7,6 @@ import State_Props from './State_Props/State_Props'
 import Props_validation from './Props_validation/Props_validation'
 import SetState from './SetState/SetState'
 import DOM_Node from './DOM_Node/DOM_Node'
+import Child_Parent from './Data_childToParent/Child_Parent'
 
-ReactDOM.render(<DOM_Node/>, document.getElementById('root'));
+ReactDOM.render(<Child_Parent/>, document.getElementById('root'));
