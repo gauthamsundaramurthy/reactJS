ae2f09b629f90f1eac15ce4fbbea74522c63067e
Gautham.S <gautham.s@tringapps.com>
2018-04-27 11:48:17 +0530

    Set state & find DOM Node

---
+++ src/DOM_Node/DOM_Node.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class DOM_Node extends React.Component {
+   constructor() {
+      super();
+      this.findDomNodeHandler = this.findDomNodeHandler.bind(this);
+   };
+   findDomNodeHandler() {
+      var myDiv = document.getElementById('myDiv');
+      ReactDOM.findDOMNode(myDiv).style.color = 'green';
+   }
+   render() {
+      return (
+         <div>
+            <button onClick = {this.findDomNodeHandler}>FIND DOME NODE</button>
+            <div id = "myDiv">NODE</div>
+         </div>
+      );
+   }
+}
+
+export default DOM_Node;
--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ import State from './State/State';
 import Props from './Props/Props'
 import State_Props from './State_Props/State_Props'
 import Props_validation from './Props_validation/Props_validation'
+import SetState from './SetState/SetState'
+import DOM_Node from './DOM_Node/DOM_Node'
 
-ReactDOM.render(<Props_validation/>, document.getElementById('root'));
+ReactDOM.render(<DOM_Node/>, document.getElementById('root'));
