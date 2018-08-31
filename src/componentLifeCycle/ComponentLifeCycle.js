0c132179db25755f01df1d6ea1e86b0ef10753a3
Gautham.S <gautham.s@tringapps.com>
2018-08-31 11:17:40 +0530

    Passing Data from Child to parent and parent to child

--- src/componentLifeCycle/ComponentLifeCycle.js
+++ src/componentLifeCycle/ComponentLifeCycle.js
@@ -1,48 +1,48 @@
 import React, { Component } from 'react';
 
 class ComponentLifeCycle extends React.Component {
-   constructor(props) {
-      super(props);
+  constructor (props) {
+    super(props)
       
-      this.state = {
-         data: 0
-      }
-      this.setNewNumber = this.setNewNumber.bind(this)
-   };
-   setNewNumber() {
-      this.setState({data: this.state.data + 1})
-   }
-   render() {
-      return (
-         <div>
-            <button onClick = {this.setNewNumber}>INCREMENT</button>
-            <Content myNumber = {this.state.data}></Content>
-         </div>
-      );
-   }
+    this.state = {
+      data: 0
+    }
+    this.setNewNumber = this.setNewNumber.bind(this)
+  };
+  setNewNumber () {
+    this.setState({data: this.state.data + 1})
+  }
+  render () {
+    return (
+      <div>
+        <button onClick={this.setNewNumber}>INCREMENT</button>
+        <Content myNumber={this.state.data}></Content>
+      </div>
+    );
+  }
 }
 class Content extends React.Component {
-   componentWillMount() {
-      console.log("...........")
-      console.log('Component WILL MOUNT!')
-   }
-   componentDidMount() {
-      console.log("...........")
-      console.log('Component DID MOUNT!')
-   }
-   componentWillReceiveProps(newProps) {   
-      console.log("...........") 
-      console.log('Component WILL RECIEVE PROPS!')
-      console.log(newProps)
-   }
-   shouldComponentUpdate(newProps, newState) {
+  componentWillMount () {
+    console.log('...........')
+    console.log('Component WILL MOUNT!')
+  }
+  componentDidMount() {
+    console.log("...........")
+    console.log('Component DID MOUNT!')
+  }
+  componentWillReceiveProps(newProps) {   
     console.log("...........") 
-    console.log('should component update')
-    console.log(newProps) 
-    console.log(newState)
-      return true;
-   }
-   componentWillUpdate(nextProps, nextState) {
+    console.log('Component WILL RECIEVE PROPS!')
+    console.log(newProps)
+  }
+  shouldComponentUpdate(newProps, newState) {
+  console.log("...........") 
+  console.log('should component update')
+  console.log(newProps) 
+  console.log(newState)
+    return true;
+  }
+  componentWillUpdate(nextProps, nextState) {
     console.log("...........")  
     console.log('Component WILL UPDATE!');
     console.log(nextProps)
